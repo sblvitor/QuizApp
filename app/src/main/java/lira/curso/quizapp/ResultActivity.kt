647a7ec113b2536e6694d165030327ef35a9201d
Vitor <sblvitor@gmail.com>
2022-01-31 11:08:36 -0400

    mensagem results dinamica

--- app/src/main/java/lira/curso/quizapp/ResultActivity.kt
+++ app/src/main/java/lira/curso/quizapp/ResultActivity.kt
@@ -11,12 +11,21 @@ class ResultActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
+        val tvMessage: TextView = findViewById(R.id.tvMessage)
         val tvName: TextView = findViewById(R.id.tvName)
         val tvScore: TextView = findViewById(R.id.tvScore)
         val btnFinish: Button = findViewById(R.id.btnFinish)
 
+        val correctAnswers = intent.getIntExtra(Constants.CORRECT_ANSWERS, 0)
+        when(correctAnswers){
+            in 0..3 -> tvMessage.text = "You're trash!!"
+            in 4..7 -> tvMessage.text = "Hey, you can do better!"
+            in 8..10 -> tvMessage.text = "WOW! Congrats!!"
+        }
+
         tvName.text = intent.getStringExtra(Constants.USER_NAME)
-        tvScore.text = "Your score is ${intent.getIntExtra(Constants.CORRECT_ANSWERS, 0)} out of ${intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0)}"
+
+        tvScore.text = "Your score is $correctAnswers out of ${intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0)}"
 
         btnFinish.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
